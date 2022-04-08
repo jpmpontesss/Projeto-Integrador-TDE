c6694413ddcb7acef1ac7abf4e75812618826929
IsaqueMansur <isaquemansur@gmail.com>
2022-04-08 11:08:25 -0300

    ajustes script tela de cadastro

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,17 @@
-import 'core-js'
-import 'regenerator-runtime/runtime';
-import './assets/css/style.css';
+let btnSignin = document.querySelector("#signin");
+let btnSignup = document.querySelector("#signup");
 
+let body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+});
+
+/* import 'core-js'
+import 'regenerator-runtime/runtime';
+import './assets/css/style.css'; */
