70d4833fe69664130f214879c55cfea18cd64470
IsaqueMansur <91393713+IsaqueMansur@users.noreply.github.com>
2022-04-11 21:40:53 -0300

    Integração backend 2

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,19 @@
-let btnSignin = document.querySelector("#signin");
-let btnSignup = document.querySelector("#signup");
+import 'core-js'
+import 'regenerator-runtime/runtime';
+import './assets/css/style.css';
+/* import './assets/css/cadastro.css'; */
+
+/* let btnSignin = document.querySelector("#signin");
+let btnSignup = document.querySelector("#signup"); */
 
 let body = document.querySelector("body");
 
 
-btnSignin.addEventListener("click", function () {
+/* btnSignin.addEventListener("click", function () {
    body.className = "sign-in-js"; 
 });
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
-});
+}); */
 
-/* import 'core-js'
-import 'regenerator-runtime/runtime';
-import './assets/css/style.css'; */
