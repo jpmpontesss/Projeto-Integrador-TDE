f24ab14a944a60aa6500607beb609e86e9165257
= <=>
2022-04-24 16:18:14 -0300

    eventos tela login OK

--- frontend/main.js
+++ frontend/main.js
@@ -3,16 +3,7 @@ import 'regenerator-runtime/runtime';
 import './assets/css/style.css';
 import './assets/css/cadastro.css';
 
-/* let btnSignin = document.querySelector("#signin");
-let btnSignup = document.querySelector("#signup"); */
+import Login from './modules/Login';
+const eventos = new Login();
 
-let body = document.querySelector("body");
-
-
-/* btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
-});
-
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-}); */
+eventos.init();
